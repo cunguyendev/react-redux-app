f4d92c0e6d38900e0db92f25b9283497109f54c2
thuyct99 <thuycao625@gmail.com>
2020-08-24 10:24:37 +0700

    Add the base assets.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,13 @@
-console.log('Hello world! Parcel - The app');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
